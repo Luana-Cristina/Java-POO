6910695f7e79dbab00aa12fd7f8438f73bab0525
Luana-Cristina <luanacristina200@gmail.com>
2021-05-31 21:46:24 -0300

    Semana 4

---
+++ P1/src/p1/B.java
@@ -0,0 +1,17 @@
+
+package p1;
+
+class B {
+	static void f() {
+		System.out.println("Método f (sem modificador)");
+		A.g();
+		int x = A.k;
+		System.out.println("Valor de x = " + x);
+		A.k = 10;
+		System.out.println("Valor de k = " + A.k);
+	}
+	
+	public  static void main (String args[]) {
+		f();
+	} 
+}
