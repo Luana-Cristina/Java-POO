cafceaab34d3d2b742ecafa7e2ef2b96f13f2f11
Luana-Cristina <luanacristina200@gmail.com>
2021-06-09 21:22:22 -0300

    opt

---
+++ Opt/src/opt/Opt.java
@@ -0,0 +1,22 @@
+
+package opt;
+
+
+public class Opt {
+
+    
+    public static void main(String[] args) {
+    int opt = 3;
+    switch(opt){
+    case 1:
+        System.out.println("primeiro");
+    case 2:
+        System.out.println("segundo");
+    case 3:
+        System.out.println("terceiro");
+    default:
+        System.out.println("qualquer posição");
+    }
+    }
+    
+}
