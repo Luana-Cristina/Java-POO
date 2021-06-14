a9aa024fc4d3094869adcecbfa3feeb11b299ded
Luana-Cristina <luanacristina200@gmail.com>
2021-06-14 20:47:22 -0300

    teste

---
+++ T/src/t/T.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t;
+
+/**
+ *
+ * @author Luana Cristina
+ */
+public class T {
+
+    int opt = 3;
+    switch(opt){
+    case 1:
+        System.out.println("primeiro");
+        break;
+    case 2 :
+    }
+    public static void main(String[] args) {
+    
+    }
+    
+}
