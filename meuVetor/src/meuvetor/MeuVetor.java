51f6ac6f0a9047ac5d7b3fd499fc3ee5e7e797f4
Luana-Cristina <luanacristina200@gmail.com>
2021-06-16 22:14:19 -0300

    vetor

---
+++ meuVetor/src/meuvetor/MeuVetor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meuvetor;
+
+
+class meuVetor {
+    
+    public meuVetor() {
+        int[] meuVetor = new int [5];
+   
+ 
+	meuVetor[0] = 33;
+	meuVetor[1] = 47;
+	meuVetor[2] = 78;
+	meuVetor[3] = 5;
+	meuVetor[4] = 18;
+		
+	for(int i=0; i< meuVetor.length; i++)
+	System.out.println("meuVetor[" + i + "] = " + meuVetor[i]);
+    }
+// meuVetor.length = comprimento do vetor = 5 inteiros mantidos no vetor
+
+    }
+
+    
+
